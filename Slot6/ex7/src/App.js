b865a1053dad2a1bb5c50ffc866799ce5fef6304
minhngucode <caoscute2805@gmail.com>
2025-02-08 16:05:55 +0700

    ex7

---
+++ Slot6/ex7/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import CardsLayout from './Components/Card';
+
+function App() {
+  return (
+    <>
+    <CardsLayout/>
+    </>
+  );
+}
+
+export default App;
